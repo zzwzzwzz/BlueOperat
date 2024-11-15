017cacd27fe656f0b220e616ec061366c4b26d95
ZZ <metiszhou@gmail.com>
2024-11-15 13:57:06 +1100

    Initial Commit

---
+++ BlueOperat/BlueOperatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueOperatApp.swift
+//  BlueOperat
+//
+//  Created by Ziwen Zhou on 15/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct BlueOperatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlueOperat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlueOperat
+//
+//  Created by Ziwen Zhou on 15/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
